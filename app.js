f2bc91d1323c09658c214291027774d4d63c75ff
Tom <poci14@gmail.com>
2020-11-15 16:53:34 -0600

    added logic for moving icon in js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+const icon = document.getElementById('moveableIcon');
+let isMoving = false;
+let x, y;
+
+icon.onmousedown = event => {
+  isMoving = true;
+  x = event.offsetX;
+  y = event.offsetY;
+};
+
+document.onmousemove = event => {
+  if (isMoving == true) {
+    console.log(x, y)
+    icon.setAttribute("style", `left:${x}px; top: ${y}px;`);
+    x = event.offsetX;
+    y = event.offsetY;
+  }
+}
+
+document.onmouseup = () => {
+  isMoving = false;
+  icon.setAttribute("style", `left:${x}px; top: ${y}px;`);
+}
